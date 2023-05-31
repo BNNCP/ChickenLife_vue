c570ce6885b94046257dc50daf835777b6235f97
z7016187 <asd880810@gmail.com>
2023-05-31 23:12:59 +0800

    fuck

--- src/js/back.js
+++ src/js/back.js
@@ -204,7 +204,11 @@ export const phas = () => {
     }
     let ice = {
         "iceServers": [
-            { "url": "stun:stun.l.google.com:19302" }
+            { "url": "stun:stun.l.google.com:19302" },
+            { "url": "stun:stun1.l.google.com:19302" },
+            { "url": "stun:stun2.l.google.com:19302" },
+            { "url": "stun:stun3.l.google.com:19302" },
+            { "url": "stun:stun4.l.google.com:19302" }
         ]
     };
     let constraints = {
